2c1ed4d472158d79c780c92e957818ea694a843d
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-07-18 22:51:38 +0530

    code added

--- js/script.js
+++ js/script.js
@@ -70,4 +70,60 @@ let progressreact = setInterval(() => {
     }
 }, rjsspeed);
 
+//filter using javascript
+$(document).ready(function(){
+    $(".filter-item").click(function(){
+        const value = $(this).attr("data-filter");
+        if(value == "all"){
+            $(".post").show("1000");
+        }else{
+            $(".post")
+              .not("." + value)
+              .hide("1000");
+            $(".post")
+              .filter("." + value)
+              .show("1000");
+        }
+    });
+});
 
+//javascript for sticky navbar even if u scroll the navbar will be fixed
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar-top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar-top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+
+// adding funtionality to back to top button 
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click",function(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
