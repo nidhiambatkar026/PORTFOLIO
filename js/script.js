5e0eef71c90ba20efa66f905ea1f63cda7b12633
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-07-18 19:36:10 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+//htmlcss progress circular bar
+let htmlProgress = document.querySelector(".html-css"),
+htmlValue = document.querySelector(".html-progress");
+
+let htmlStartValue = 0, htmlEndValue = 90,htmlspeed = 30;
+let progresshtml = setInterval(() => {
+    htmlStartValue++;
+
+    htmlValue.textContent = `${htmlStartValue}%`;
+    htmlProgress.style.background = `conic-gradient(#fca61f ${
+        htmlStartValue * 3.6
+    }deg, #ededed 0deg)`;
+
+    if(htmlStartValue == htmlEndValue){
+        clearInterval(progresshtml);
+    }
+}, htmlspeed);
+
+//javascriptProgress Circular bar
+let javascriptProgress = document.querySelector(".javascript"),
+javascriptValue = document.querySelector(".javascript-progress");
+
+let javascriptStartValue = 0, javascriptEndValue = 75,javascriptspeed = 30;
+let progressjs = setInterval(() => {
+    javascriptStartValue++;
+
+    javascriptValue.textContent = `${javascriptStartValue}%`;
+    javascriptProgress.style.background = `conic-gradient(#7d2ae8 ${
+        javascriptStartValue * 3.6
+    }deg, #ededed 0deg)`;
+
+    if(javascriptStartValue == javascriptEndValue){
+        clearInterval(progressjs);
+    }
+}, jsspeed);
+
+//php progress circular bar
+let phpProgress = document.querySelector(".javascript"),
+phptValue = document.querySelector(".javascript-progress");
+
+let phpStartValue = 0, phpEndValue = 75,phpspeed = 30;
+let progressphp = setInterval(() => {
+    phpStartValue++;
+
+    phpValue.textContent = `${phpStartValue}%`;
+    phpProgress.style.background = `conic-gradient(#20c997 ${
+        phpStartValue * 3.6
+    }deg, #ededed 0deg)`;
+
+    if(phpStartValue == phpEndValue){
+        clearInterval(progressphp);
+    }
+}, phpspeed);
+
+//reactjs progress circular bar
+
+
